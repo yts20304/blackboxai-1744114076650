52a62f02b3e8b2210268c3cf3210f8859ab9cd6a
yts20304 <yts20304@gmail.com>
2025-04-09 10:01:38 +0530

    Add traffic-generator-bot/index.js 1744173097557

--- traffic-generator-bot/index.js
+++ traffic-generator-bot/index.js
@@ -1,8 +1,18 @@
 const { logger } = require('./src/logger');
+const { Telegraf } = require('telegraf');
 const TrafficEngine = require('./src/trafficEngine');
 const AdSimulator = require('./src/adSimulator');
 const Scheduler = require('./src/scheduler');
 const config = require('./config.json');
+
+const bot = new Telegraf(config.telegram.token);
+
+bot.start((ctx) => {
+  ctx.reply('Welcome! The bot is now running.');
+  logger.info('Bot started command received');
+});
+
+bot.launch();
 
 (async () => {
   try {
